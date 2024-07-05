e6037bce0868bf0ce95458bd723479aed202a163
Manas Kolaskar <148738351+maannas@users.noreply.github.com>
2024-07-05 11:20:51 +0000

    feedback and footer done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,46 @@
 'use client';
 
+import { motion } from "framer-motion";
+import { socials } from "../constants"
+import styles from '../styles';
+import { footerVariants } from '../utils/motion'
 const Footer = () => (
-  <footer>
-    footer
-  </footer>
+  <motion.footer
+  variants={footerVariants}
+  initial="hidden"
+  whileInView="show"
+  className={`${styles.paddings} py-8 relative`}
+  >
+    <div className="footer-gradient" />
+
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className="flex items-center justify-between flex-wrap gap-5">
+        <h4 className="font-bold md:text-[64px] text-[44px] text-white">
+          Connect with us
+        </h4>
+      </div>
+       <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+
+        <div className="flex items-center justify-between flex-wrap gap-4">
+         <h4 className="font-extrabold text-white text-[24px]">Somaiya Innovation and Impact Festival</h4>
+         <p className="font-normal text-[14px] text-white opacity-50">
+          Copyright@ &nbsp;2024-2025&nbsp; SIIF. All Rights reserved
+         </p>
+         <div className="flex gap-4">
+          {socials.map((social) =>(
+            <img 
+             key={social.name}
+             src={social.url}
+             alt={social.name}
+             className="w-[24px] h-[24px] object-contain cursor-pointer"
+            />
+          ))}
+         </div>
+        </div>
+       </div>
+    </div>
+  </motion.footer>
 );
 
 export default Footer;
--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,46 @@
 'use client';
 
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { fadeIn, staggerContainer, zoomIn } from "../utils/motion";
+
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+  <motion.div
+  variants={staggerContainer}
+  initial="hidden"
+  whileInView="show"
+  viewport={{ once: 'false', amount: 0.25}}
+  className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row gap-6`}
+  >
+  <motion.div
+  variants={fadeIn('right', 'tween', 0.2, 1)}
+  className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
+  >
+  <div className="feedback-gradient" />
+  <div>
+    <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">
+      Hetal
+    </h4>
+    <p className="mt-[8px] sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white">
+      Founder | Somaiya Innovation and Space Festival
+    </p>
+  </div>
+  <p className="mt-[24px] sm:text-[28px] text-[18px] sm:leading-[45px] leading-[39px] text-white">
+  Join us on this exhilarating journey of exploration, discovery, and innovation at the Somaiya Innovation and Impact Festival! Together, let’s create, collaborate, and make a difference.
+  </p>
+  </motion.div>
+<motion.div
+variants={fadeIn('left', 'tween',0.2, 1)}
+className="relative flex-1 flex justify-center items-center"
+>
+  <img src="/planet-09.png"
+  alt="planet" className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
+   />
+ 
+</motion.div>
+
+  </motion.div>
   </section>
 );
 
